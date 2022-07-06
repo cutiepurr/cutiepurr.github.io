d0c9a4f9ca4f2790c5a622603c76e6000b99cd20
Nguyễn Thị Tú Linh (Gracie) <linhtunguyen.1311@gmail.com>
2022-07-06 13:57:33 +0930

    Change image

--- src/shared/about.js
+++ src/shared/about.js
@@ -1,5 +1,5 @@
 export const ABOUT = {
     name: "Thi Tu Linh (Linh) Nguyen",
-    image: "assets/images/avatar.png",
+    image: "assets/images/LinhNguyen.png",
     description: "Hello, my name is Linh. I'm doing Bachelor of Computer Science at the University of Adelaide (2022 - 2024)."
 }
