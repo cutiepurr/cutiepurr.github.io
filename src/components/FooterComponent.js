709393f77db7b34dbf66677401faa4bc962ca373
Nguyễn Thị Tú Linh (Gracie) <linhtunguyen.1311@gmail.com>
2022-07-06 14:19:24 +0930

    change palette

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,7 +4,7 @@ export function Footer(props) {
     return (
         <div className='container footer text-center mt-5'>
             <a className='foot icons' href="mailto:linhtunguyen.1311@gmail.com"><i className="fa fa-envelope"></i></a>
-            <a className='foot icons' href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
+            <a className='foot icons' href="http://www.linkedin.com/in/nguyen-thi-tu-linh"><i className="fa fa-linkedin"></i></a>
             <a className='foot icons' href="https://github.com/cutiepurr"><i className="fa fa-github"></i></a>
         </div>
     );
