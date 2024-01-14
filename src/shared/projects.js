b51461c61c7ac9ac84c0f7b17ca084ba8237e61b
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-01-14 19:48:46 +1030

    Change project links

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -1,22 +1,22 @@
 export const PROJECTS = [
     {
         name: "2048 Game Clone",
-        github: "https://github.com/cutiepurr/2048-game",
-        website: "https://cutiepurr.github.io/2048-game/",
+        github: "https://github.com/ttlinhnguyen/2048-game",
+        website: "https://ttlinhnguyen.github.io/2048-game/",
         description: "JavaScript, Phaser",
         thumbnail: "assets/2048.png"
     },
     {
         name: "Social Student Clubs",
-        github: "https://github.com/cutiepurr/social-student-clubs",
+        github: "https://github.com/ttlinhnguyen/social-student-clubs",
         website: "https://social-student-clubs.onrender.com/",
         description: "JavaScript, Vue, Express, Node, MySQL",
         thumbnail: "assets/Clubs.png"
     },
     {
         name: "Portfolio Website",
-        github: "https://github.com/cutiepurr/cutiepurr.github.io",
-        website: "https://cutiepurr.github.io/",
+        github: "https://github.com/ttlinhnguyen/cutiepurr.github.io",
+        website: "https://ttlinhnguyen.github.io/",
         description: "JavaScript, React",
         thumbnail: "assets/Portfolio.png"
     }
