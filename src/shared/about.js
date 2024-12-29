0b5bc821a9aedb4a0f1c3ae7d971d8c3c8f70f3f
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-12-29 22:41:08 +1030

    Fix ESLint issues

--- src/shared/about.js
+++ src/shared/about.js
@@ -1,5 +1,7 @@
 export const ABOUT = {
     name: "Linh Nguyen",
     image: "assets/LinhNguyen.jpeg",
-    description: `Hi, I'm Linh, and I'm a software engineer with an interest in full-stack development and distributed systems. I love the constant challenge of learning new things and facing complex software problems.`
+    description: `Hi, I'm Linh, and I'm a software engineer with 
+    an interest in full-stack development and distributed systems. 
+    I love the constant challenge of learning new things and facing complex software problems.`
 };
