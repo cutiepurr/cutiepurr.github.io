41afd8ff89147b03211a724a9110c6563ca0afbe
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-12-30 03:23:58 +1030

    Sticky headings

--- src/components/SectionTemplate.js
+++ src/components/SectionTemplate.js
@@ -3,9 +3,13 @@ import React from "react";
 
 const SectionTemplate = ({icon="", heading, ...props}) => {
     return (
-        <div className={`grid sm:grid-cols-12 gap-1 ${props.className ?? ""}`} {...props}>
-            <div className="col-span-1 text-3xl">{icon}</div>
-            <h2 className="col-span-3">{heading}</h2>
+        <div className={`grid sm:grid-cols-12 gap-2 ${props.className ?? ""}`} {...props}>
+            <div className="col-span-1 text-3xl sticky top-4 z-10">
+                <div className="sticky top-4">{icon}</div>
+            </div>
+            <h2 className="col-span-3">
+                <div className="sticky top-4">{heading}</div>
+            </h2>
             <div className="col-span-8 mx-auto sm:mx-0">{props.children}</div>
         </div>
     );
